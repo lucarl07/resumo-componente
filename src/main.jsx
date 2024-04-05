7fb97483d1a976419ef087efdbfed4cc5a6e7810
lucarl07 <junitinho2007@gmail.com>
2024-04-05 11:26:33 -0300

    Configurar propriedades básicas no CSS e testá-las

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
+/*** MAIN REACT FILE ***/
+/* eslint-disable no-unused-vars */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div style={{width: '500px', height: '500px', backgroundColor: 'var(--light-red)'}}>
+      Lorem ipsum dolor sit amet.
+    </div>
   </React.StrictMode>,
 )
